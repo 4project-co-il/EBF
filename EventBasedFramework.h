644893dd084b368eb4ea45efe073a06ecdd826e7
Alex <alex@4project.co.il>
2025-02-15 14:14:50 +0200

    Adding .h file with the library name as arduino-lint requires

---
+++ EventBasedFramework.h
@@ -0,0 +1,6 @@
+#ifndef __EBF_H__SHORTCUT_
+#define __EBF_H__SHORTCUT_
+
+#include "src/EBF.h"
+
+#endif
---
+++ src/EventBasedFramework.h
@@ -0,0 +1,23 @@
+#ifndef __EBF_H__
+#define __EBF_H__
+
+#include <Arduino.h>
+
+#if __has_include("EBF_Config.h")
+	#include "EBF_Config.h"
+#endif
+
+#include "Core/EBF_Core.h"
+#include "Core/EBF_Logic.h"
+#include "Core/EBF_Timers.h"
+#include "Core/EBF_MessageQueue.h"
+#include "Core/EBF_HalInstance.h"
+
+#include "Core/EBF_DigitalInput.h"
+#include "Core/EBF_DigitalOutput.h"
+#include "Core/EBF_PwmOutput.h"
+#include "Core/EBF_AnalogInput.h"
+#include "Core/EBF_Serial.h"
+#include "Core/EBF_I2C.h"
+
+#endif
