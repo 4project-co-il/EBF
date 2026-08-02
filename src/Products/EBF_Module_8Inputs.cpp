043dccc918dc846b6e259d78b6be7be8984ab5e8
Alex <alex@4project.co.il>
2026-08-02 18:02:19 +0300

    Adding 8 inputs module implementation

---
+++ src/Products/EBF_Module_8Inputs.cpp
@@ -0,0 +1,266 @@
+#include "EBF_Module_8Inputs.h"
+#include "../Core/EBF_Logic.h"
+#include "../Core/EBF_DigitalInput.h"
+#include "../Core/EBF_Core.h"
+
+extern void EBF_EmptyCallback();
+
+// Initializing EBF_Module_8Inputs class instance.
+// The i2cAddress should specify the device I2C address
+uint8_t EBF_Module_8Inputs::Init(uint8_t i2cAddress)
+{
+	uint8_t rc;
+
+	chip.i2cAddress = i2cAddress;
+
+	for (uint8_t i=0; i<numberOfInputs; i++) {
+		onChangeCallback[i] = EBF_EmptyCallback;
+	}
+
+	rc = EBF_HalInstance::Init(HAL_Type::I2C_INTERFACE, i2cAddress);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	// Configure the chip to all inputs, latch enable and interrupt mask enabled
+	rc = chip.SetConfiguration(0xFF);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	rc = chip.SetLatching(0xFF);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	// Enable all 8bits since it's a 8input module implementation
+	rc = chip.SetInterruptMask(0x00);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	// Get initial input lines status and reset the interrupts
+	rc = chip.GetInput(lastValues);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	return EBF_OK;
+}
+
+#ifdef EBF_USE_INTERRUPTS
+uint8_t EBF_Module_8Inputs::AttachInterrupt(uint8_t interruptPin)
+{
+	uint8_t rc;
+	EBF_Logic *pLogic = EBF_Logic::GetInstance();
+
+	rc = pLogic->AttachInterrupt(interruptPin, this, EBF_DigitalInput::InterruptMode::MODE_LOW);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	// No polling is needed when interrupt is used
+	SetPollingInterval(EBF_NO_POLLING);
+
+	return EBF_OK;
+}
+#endif
+
+// Called to process the instance after pollInterval
+uint8_t EBF_Module_8Inputs::Process()
+{
+	EBF_Logic *pLogic = EBF_Logic::GetInstance();
+	PostponedInterruptData data = {0};
+
+#ifdef EBF_USE_INTERRUPTS
+	// Process interrupt detected logic
+	if (pLogic->IsPostInterruptProcessing()) {
+		data.uint32 = pLogic->GetLastMessageParam1();
+
+		// Set current interface provider and event index before the callbacks are called
+		currentEventIndex = data.fields.index;
+		lastValues = data.fields.event;
+
+		// Callback from the normal run mode
+		ExecuteCallback();
+	}
+#else
+	// No interrupts are used, read the interrupt register to see if anything should be processed
+	uint8_t rc;
+	uint8_t intStatus;
+
+	// Read interrupt register from the chip to know what input fired the interrupt
+	rc = chip.GetInterruptStatus(intStatus);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return;
+	}
+
+	// We have something to process
+	if (intStatus != 0) {
+		// Read current inputs, it will reset the interrupts
+		rc = chip.GetInput(lastValues);
+		if (rc != EBF_OK) {
+			EBF_REPORT_ERROR(rc);
+			return;
+		}
+
+		// Loop on all the lines to find the changes. Several lines might change together
+		for (currentEventIndex=0; currentEventIndex<numberOfInputs; currentEventIndex++) {
+			if (intStatus & 1<<currentEventIndex) {
+				ExecuteCallback();
+			}
+
+			// Since most likely the interrupts will not arrive at the same time for several ports
+			// It makes sence to check if there are no more interrupts to process and exit the loop
+			intStatus &= ~(1<<currentEventIndex);
+
+			if (intStatus == 0) {
+				break;
+			}
+		}
+	}
+#endif
+
+	return EBF_OK;
+}
+
+#ifdef EBF_USE_INTERRUPTS
+void EBF_Module_8Inputs::ProcessInterrupt()
+{
+	uint8_t rc;
+	uint8_t currentInputs;
+	uint8_t changedLines;
+	uint8_t stillProcessing = true;
+
+	// The loop will continue while processing is still needed
+	while (stillProcessing) {
+		// clear the processing flag. it will be set on every processing pass, so another verification loop will be done
+		stillProcessing = false;
+
+		// Get current inputs
+		rc = chip.GetInput(currentInputs);
+		if (rc != EBF_OK) {
+			EBF_REPORT_ERROR(rc);
+			return;
+		}
+
+		// Process detected changes
+		changedLines = currentInputs ^ lastValues;
+
+		lastValues = currentInputs;
+
+		// There are changes
+		if (changedLines != 0) {
+			// Loop on all the bits to find what changed
+			for (uint8_t i=0; i<numberOfInputs; i++) {
+				if (changedLines & 1<<i) {
+					// Found the change
+					stillProcessing = true;
+
+#ifdef EBF_DIRECT_CALL_FROM_ISR
+					// Set currentEventIndex and lastValues before actual call to the callbacks
+					currentEventIndex = i;
+					ExecuteCallback();
+#else
+					// Postpone the processing so the event will be handled from the normal run
+					PostponeProcessing(i, currentInputs);
+#endif
+
+					// Since most likely the interrupts will not arrive at the same time for several ports
+					// It makes sence to check if there are no more interrupts to process and exit the loop
+					changedLines &= ~(1<<i);
+
+					if (changedLines == 0) {
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+// PostponeProcessing should be called to postpone the callback processing later in the normal loop
+uint8_t EBF_Module_8Inputs::PostponeProcessing(uint8_t eventIndex, uint8_t inputValues)
+{
+	uint8_t rc;
+	EBF_Logic *pLogic = EBF_Logic::GetInstance();
+	PostponedInterruptData data = {0};
+
+	data.fields.index = eventIndex;
+	data.fields.event = inputValues;
+
+	// Pass the control back to EBF, so it will call the Process() function from normal run
+	rc = pLogic->PostponeInterrupt(this, data.uint32);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return rc;
+	}
+
+	return EBF_OK;
+}
+#endif
+
+uint8_t EBF_Module_8Inputs::SetOnChange(uint8_t index, EBF_CallbackType onChangeCallback)
+{
+	if (index >= numberOfInputs) {
+		EBF_REPORT_ERROR(EBF_INDEX_OUT_OF_BOUNDS);
+		return EBF_INDEX_OUT_OF_BOUNDS;
+	}
+
+	this->onChangeCallback[index] = onChangeCallback;
+
+	return EBF_OK;
+}
+
+// Returns current value of the specified input line
+uint8_t EBF_Module_8Inputs::GetValue(uint8_t index)
+{
+	if (GetValues() & 1<<index) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+// Returns current values of all the input lines
+uint8_t EBF_Module_8Inputs::GetValues()
+{
+	uint8_t rc;
+	uint8_t values;
+
+	rc = chip.GetInput(values);
+	if (rc != EBF_OK) {
+		EBF_REPORT_ERROR(rc);
+		return 0;
+	}
+
+	return values;
+}
+
+// Returns last value of the specified input line as it appeared while reading from the chip
+uint8_t EBF_Module_8Inputs::GetLastValue(uint8_t index)
+{
+	if (lastValues & 1<<index) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+// Returns the value of the input line as it was registered during last interrupt
+uint8_t EBF_Module_8Inputs::GetLastValues()
+{
+	return lastValues;
+}
+
+void EBF_Module_8Inputs::ExecuteCallback()
+{
+	onChangeCallback[currentEventIndex]();
+}
---
+++ src/Products/EBF_Module_8Inputs.h
@@ -0,0 +1,82 @@
+#ifndef __EBF_MODULE_8INPUTS_H__
+#define __EBF_MODULE_8INPUTS_H__
+
+#include <Arduino.h>
+#if __has_include("Project_Config.h")
+	#include "Project_Config.h"
+#endif
+
+#include "../Core/EBF_Global.h"
+#include "../Core/EBF_Logic.h"
+#include "../Core/EBF_HalInstance.h"
+#include "../HAL/EBF_HAL_TCAL9538.h"
+
+class EBF_Module_8Inputs : public EBF_HalInstance {
+	private:
+		EBF_DEBUG_MODULE_NAME("EBF_Module_8Inputs");
+
+	public:
+		EBF_Module_8Inputs(EBF_I2C &i2cInterface) : chip(i2cInterface) {}
+		EBF_Module_8Inputs(EBF_I2C* pI2cInterface) : chip(pI2cInterface) {}
+
+		static const uint8_t numberOfInputs = 8;
+
+		uint8_t Init(uint8_t i2cAddress = EBF_HAL_TCAL9538::defaultI2CAddress);
+
+		// Returns current value of the specified input line
+		uint8_t GetValue(uint8_t index);
+		// Returns current values of all the input lines
+		uint8_t GetValues();
+		// Returns last value of the specified input line as it appeared while reading from the chip
+		uint8_t GetLastValue(uint8_t index);
+		// Returns all last values as it appeared while reading from the chip
+		uint8_t GetLastValues();
+
+#ifdef EBF_USE_INTERRUPTS
+		// Call to attach the device to an interrupt line
+		uint8_t AttachInterrupt(uint8_t interruptPin);
+		uint8_t PostponeProcessing(uint8_t eventIndex, uint8_t inputValues);
+		uint8_t InInterrupt() {
+			EBF_Logic *pLogic = EBF_Logic::GetInstance();
+			return pLogic->IsRunFromIsr();
+		}
+#else
+		uint8_t PostponeProcessing() { return EBF_INVALID_STATE; }
+		uint8_t InInterrupt() { return 0; }
+#endif
+
+		uint8_t SetOnChange(uint8_t index, EBF_CallbackType onChangeCallback);
+
+		// Returns input index that caused the callback function call
+		// You can have the same callback function for all the inputs events
+		// where you can call the GetEventIndex to know which input actually changed
+		uint8_t GetEventIndex() { return currentEventIndex; }
+
+		typedef union {
+			struct {
+				uint32_t index : 3;		// up to 8 inputs
+				uint32_t event : 8;		// input event that should be executed
+				uint32_t reserved : 21;
+			} fields;
+			uint32_t uint32;
+		} PostponedInterruptData;
+
+	protected:
+		// I2C I/O ports chip
+		EBF_HAL_TCAL9538 chip;
+
+		// Callbacks
+		EBF_CallbackType onChangeCallback[numberOfInputs];
+
+		uint8_t lastValues;
+		uint8_t currentEventIndex;
+
+		virtual uint8_t Process();
+		virtual void ExecuteCallback();
+
+#ifdef EBF_USE_INTERRUPTS
+		void ProcessInterrupt();
+#endif
+};
+
+#endif
